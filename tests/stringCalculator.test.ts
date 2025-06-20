542cf61cb1581abbdce3b24a0a0bebe4d746dc01
Kritika <kritikam77@gmail.com>
2025-06-20 23:37:53 +0530

    test: return number for single input value

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -1,7 +1,14 @@
 import {add} from '../src/stringCalculator';
 
 describe('String Calculator',()=>{
+
+    //Test for empty string input
     test('returns 0 for empty string input',()=>{
         expect(add("")).toBe(0)
+    })
+
+    //test for single number input
+    test('returns number for single input value',()=>{
+        expect(add("5")).toBe(5)
     })
 })
