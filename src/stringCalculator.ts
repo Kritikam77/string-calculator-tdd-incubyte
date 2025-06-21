40effb799f5556734ff54b50d019e551ba446a34
Kritika <kritikam77@gmail.com>
2025-06-21 12:08:37 +0530

    feat: support new lines as delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -4,7 +4,9 @@ export function add(numbers: string): number {
   
   //if the input is a single number, return that number
   //if input is comma-separated numbers, return sum, works for both multiple and single numbers
-  const nums=numbers.split(',').map(num=>parseInt(num))
+
+  const delimiters=/[,\n]/; //regex to split by comma or newline
+  const nums=numbers.split(delimiters).map(num=>parseInt(num))
 
   return nums.reduce((sum,curr)=>sum+curr,0)
 }
