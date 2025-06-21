d20dd81d3febd183da6f72486c921f14494fbe55
Kritika <kritikam77@gmail.com>
2025-06-21 12:06:38 +0530

    test: handle new lines between numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -22,4 +22,10 @@ describe('String Calculator',()=>{
         expect(add("1,2,3,4,5")).toBe(15)
         expect(add("10,20,30")).toBe(60)
     })
+
+    //test for handling new lines between numbers
+    test('returns sum when new lines are used as delimiters',()=>{
+        expect(add("1\n2,3")).toBe(6);
+        expect(add("4\n5\n6")).toBe(15)
+    })
 })
