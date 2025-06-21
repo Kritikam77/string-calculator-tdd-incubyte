0e4067d81c73aadf2e2c4c3c544e096ac32643d5
Kritika <kritikam77@gmail.com>
2025-06-21 12:01:03 +0530

    feat: handle any number of comma-separated values

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,9 @@
 export function add(numbers: string): number {
+  //if the input is an empty string, return 0
   if(numbers==="") return 0; 
   
+  //if the input is a single number, return that number
+  //if input is comma-separated numbers, return sum, works for both multiple and single numbers
   const nums=numbers.split(',').map(num=>parseInt(num))
 
   return nums.reduce((sum,curr)=>sum+curr,0)
